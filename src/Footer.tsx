d2c0f0541bb1673a3d1f1c18a936803d61a86aec
ndeye khady seck <Khadyseck5259@gmail.com>
2026-06-11 18:56:18 +0100

    premier commit

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,4 +1,8 @@
-function Footer() {
-  return <div>Footer</div>
+export default function Footer() {
+  return (
+    <footer className="bg-violet-400 text-white text-center py-4 mt-10">
+      <p>© 2026 - Projet Finance Personnelle</p>
+      <p>Groupe React + TypeScript</p>
+    </footer>
+  );
 }
-export default Footer;
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,4 +1,27 @@
-function Navbar() {
-  return <div>Navbar</div>
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-violet-400 text-white px-6 py-4 flex justify-between items-center">
+      <h1 className="font-bold text-xl">Finance Personelle</h1>
+
+      <div className="flex gap-4">
+        <Link to="/" className="hover:underline">
+          Dashboard
+        </Link>
+
+        <Link to="/revenus" className="hover:underline">
+          Revenus
+        </Link>
+
+        <Link to="/depenses" className="hover:underline">
+          Dépenses
+        </Link>
+
+        <Link to="/apropos" className="hover:underline">
+          À propos
+        </Link>
+      </div>
+    </nav>
+  );
 }
-export default Navbar;
